2ce26c546a38714f20a5f53225f850a00d641c6c
AbhinavPanjala <panjala.abhinav95@gmail.com>
2018-08-22 12:02:06 -0500

    Filed added

---
+++ Information cloaking source/Backup/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Steganography")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Steganography")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)
+[assembly: Guid("a28db00f-f544-4000-939b-d8ecabc3d6f4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
